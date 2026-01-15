b67e1835f23987970195d0d0f6ce83430cc958a9
dayathid12 <dayat.hidayat@unpad.ac.id>
2026-01-15 15:56:20 +0700

    Filter tab

--- app/Filament/Resources/SPTJBPengemudiResource/Pages/ListSPTJBPengemudi.php
+++ app/Filament/Resources/SPTJBPengemudiResource/Pages/ListSPTJBPengemudi.php
@@ -5,7 +5,8 @@ namespace App\Filament\Resources\SPTJBPengemudiResource\Pages;
 use App\Filament\Resources\SPTJBPengemudiResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Support\Enums\MaxWidth;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListSPTJBPengemudi extends ListRecords
 {
@@ -13,11 +14,110 @@ class ListSPTJBPengemudi extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [];
+        return [
+            // Actions\CreateAction::make(),
+        ];
     }
 
-    public function getMaxContentWidth(): ?string
+    public function getTabs(): array
     {
-        return MaxWidth::Screen->value;
+        return [
+            'semua' => Tab::make('Semua')
+                ->icon('heroicon-o-queue-list'),
+
+            'ajukan' => Tab::make('Ajukan')
+                ->icon('heroicon-o-document')
+                ->badge(function () {
+                    return static::getResource()::getEloquentQuery()
+                        ->whereHas('perjalanan', function (Builder $query) {
+                            $query->whereNotNull('upload_surat_tugas')
+                                 ->where('upload_surat_tugas', '!=', '');
+                        })
+                        ->whereDoesntHave('pengemudi', function ($q) {
+                            $q->whereExists(function ($exists) {
+                                $exists->selectRaw('1')
+                                       ->from('sptjb_uang_pengemudi_details')
+                                       ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                            });
+                        })
+                        ->whereDoesntHave('asisten', function ($q) {
+                            $q->whereExists(function ($exists) {
+                                $exists->selectRaw('1')
+                                       ->from('sptjb_uang_pengemudi_details')
+                                       ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                            });
+                        })
+                        ->count();
+                })
+                ->badgeColor('gray')
+                ->modifyQueryUsing(function (Builder $query) {
+                    $query->whereHas('perjalanan', function (Builder $subQuery) {
+                        $subQuery->whereNotNull('upload_surat_tugas')
+                                 ->where('upload_surat_tugas', '!=', '');
+                    })
+                    ->whereDoesntHave('pengemudi', function ($q) {
+                        $q->whereExists(function ($exists) {
+                            $exists->selectRaw('1')
+                                   ->from('sptjb_uang_pengemudi_details')
+                                   ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                        });
+                    })
+                    ->whereDoesntHave('asisten', function ($q) {
+                        $q->whereExists(function ($exists) {
+                            $exists->selectRaw('1')
+                                   ->from('sptjb_uang_pengemudi_details')
+                                   ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                        });
+                    });
+                }),
+
+            'selesai' => Tab::make('Selesai')
+                ->icon('heroicon-o-check-circle')
+                ->badge(function () {
+                    return static::getResource()::getEloquentQuery()
+                        ->whereHas('perjalanan', function (Builder $query) {
+                            $query->whereNotNull('upload_surat_tugas')
+                                 ->where('upload_surat_tugas', '!=', '');
+                        })
+                        ->where(function ($query) {
+                            $query->whereHas('pengemudi', function ($q) {
+                                $q->whereExists(function ($exists) {
+                                    $exists->selectRaw('1')
+                                           ->from('sptjb_uang_pengemudi_details')
+                                           ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                                });
+                            })->orWhereHas('asisten', function ($q) {
+                                $q->whereExists(function ($exists) {
+                                    $exists->selectRaw('1')
+                                           ->from('sptjb_uang_pengemudi_details')
+                                           ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                                });
+                            });
+                        })
+                        ->count();
+                })
+                ->badgeColor('success')
+                ->modifyQueryUsing(function (Builder $query) {
+                    $query->whereHas('perjalanan', function (Builder $subQuery) {
+                        $subQuery->whereNotNull('upload_surat_tugas')
+                                 ->where('upload_surat_tugas', '!=', '');
+                    })
+                    ->where(function ($query) {
+                        $query->whereHas('pengemudi', function ($q) {
+                            $q->whereExists(function ($exists) {
+                                $exists->selectRaw('1')
+                                       ->from('sptjb_uang_pengemudi_details')
+                                       ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                            });
+                        })->orWhereHas('asisten', function ($q) {
+                            $q->whereExists(function ($exists) {
+                                $exists->selectRaw('1')
+                                       ->from('sptjb_uang_pengemudi_details')
+                                       ->whereRaw('sptjb_uang_pengemudi_details.nama = stafs.nama_staf');
+                            });
+                        });
+                    });
+                }),
+        ];
     }
 }
